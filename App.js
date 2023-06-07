dfa494e08156c665adf74498bcab0d04c175b7d1
Emanuel-Reiter <emanuel.lsrei@gmail.com>
2023-06-07 14:59:12 -0300

    Commit dos arquivos

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, ImageBackground } from "react-native";
+
+import Header from "./src/components/Header/index.js"
+import Form from "./src/components/Form/index.js";
+
+const backgroundImage = {uri: "https://e1.pxfuel.com/desktop-wallpaper/793/794/desktop-wallpaper-japanese-aesthetic-list-android-aesthetic-japan.jpg"};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={backgroundImage} resizeMode="cover" style={styles.backgroundImage}>
+       <Header />
+       <Form />
+      </ImageBackground>
     </View>
   );
 }
@@ -13,8 +20,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+
+    backgroundColor: 'hsl(220, 75%, 50%)',
+
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+  },
+  backgroundImage: {
+    flex: 1,
+    width: "100%",
   },
 });
